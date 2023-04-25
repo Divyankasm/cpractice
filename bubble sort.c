8bedb70824ed0fd69902a8d3e77230711999adbd
Divyank <divyank00009856@gmail.com>
2023-04-25 23:57:21 +0530

    new programs

--- bubble sort.c
+++ bubble sort.c
@@ -2,7 +2,7 @@
 
 int main() {
     int arr[] = {5,3,8,4,2};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int n =5;
     int i, j, temp;
     printf("Before sorting: ");
     for (i = 0; i < n; i++) {
@@ -14,9 +14,7 @@ int main() {
                 temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
-            }
-        }
-    }
+            }} }
     printf("\nAfter sorting: ");
     for (i = 0; i < n; i++) {
         printf("%d ", arr[i]);
