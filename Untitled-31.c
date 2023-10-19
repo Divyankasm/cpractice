1d4ae6d31e8e84d7984f7157a073b046d72cc419
Divyank <divyank00009856@gmail.com>
2023-10-20 01:31:13 +0530

    dsa

---
+++ Untitled-31.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+   
+    for(;;)
+    {
+        printf("ENTER AN NUMBER:");
+        scanf("%d", &num);
+        printf("num=%d\n",num);
+        if(num%2!=0){
+            printf("you entered an odd number:%d\n", num);
+            
+            break;
+
+
+        }
+       
+    }
+  
+     return 0;
+}
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,25 +1,15 @@
-#include <stdio.h>
-
-int main() {
-    int arr[] = {5,3,8,4,2};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    int i, j, temp;
-    printf("Before sorting: ");
-    for (i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-    for (i = 0; i < n-1; i++) {
-        for (j = 0; j < n-i-1; j++) {
-            if (arr[j] > arr[j+1]) {
-                temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
+#include<stdio.h>
+int main()
+{ 
+    int n;
+    for(;;){
+        printf("enter number");
+        scanf("%d\n",&n);
+        printf("number is %d",n);
+        if (n%2 != 0){
+            printf("thank you%d",n);
+            break;
         }
-    }
-    printf("\nAfter sorting: ");
-    for (i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
+    } 
     return 0;
 }
